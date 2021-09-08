982ff94f2720d26e38c32b108d71fe638f13d752
SpeeedWagon <gabriel.duda@deepcode.xyz>
2021-09-08 17:40:01 +0300

    22

--- public/messaging-sw.js
+++ public/messaging-sw.js
@@ -1,6 +1,18 @@
-importScripts('https://www.gstatic.com/firebasejs/9.0.1/firebase-messaging.js')
-// const messaging = firebase.getMessaging();
+importScripts("https://www.gstatic.com/firebasejs/7.9.1/firebase-app.js");
+importScripts("https://www.gstatic.com/firebasejs/7.9.1/firebase-messaging.js");
 
-// console.log(JSON.stringify(messaging))
-console.log('indeed')
+firebase.initializeApp({
+  apiKey: "AIzaSyAmIHvAe-EOYY-TUPkcb_Byi74j7qqG9dY",
+  projectId: "pushnotifications-8d42a",
+  messagingSenderId: "677674369528",
+  appId: "1:677674369528:web:1926ecb152fcb5a8e1160d",
+});
+
+const messaging = firebase.messaging();
+
+console.log(messaging);
+messaging.getToken().then((tok) => {
+  console.log(tok);
+});
+// console.log("indeed");
 // console.log(getToken())
