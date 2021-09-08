92a1c3ef8c53229c8d9da60c53c2f4b38d9454d5
SpeeedWagon <gabriel.duda@deepcode.xyz>
2021-09-08 17:57:46 +0300

    request permission

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,17 @@
-import React, { useState } from 'react'
-import logo from './logo.svg'
-import './App.css'
-import {getApp} from 'firebase/app'
+import React, { useState } from "react";
+import logo from "./logo.svg";
+import "./App.css";
+import { getApp } from "firebase/app";
 function App() {
-  const [count, setCount] = useState(0)
+  const [count, setCount] = useState(0);
   const check = getApp().options;
-  if('serviceWorker' in navigator){
-    navigator.serviceWorker.register('/messaging-sw.js')    
+  if ("serviceWorker" in navigator) {
+    navigator.serviceWorker.register("/messaging-sw.js");
   }
-  return (
-    <code>
-      {JSON.stringify(check)}
-    </code>
-  )
+  Notification.requestPermission().then((data) => {
+    console.log("thanks for granting persimission✨");
+  });
+  return <code>{JSON.stringify(check)}</code>;
 }
 
-export default App
+export default App;
