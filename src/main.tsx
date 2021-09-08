1c8513f54ce15e4ca1be15d4561dae82fb24fdcc
SpeeedWagon <gabriel.duda@deepcode.xyz>
2021-09-08 19:40:09 +0300

    trying something

--- src/main.tsx
+++ src/main.tsx
@@ -1,20 +1,24 @@
-import React from 'react'
-import ReactDOM from 'react-dom'
-import './index.css'
-import App from './App'
-import {initializeApp} from "firebase/app";
+import React from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
+import App from "./App";
+import { initializeApp } from "firebase/app";
+import { onMessage, getMessaging } from "firebase/messaging";
 
 // Use your config values here.
-  initializeApp({
+initializeApp({
   apiKey: "AIzaSyAmIHvAe-EOYY-TUPkcb_Byi74j7qqG9dY",
   projectId: "pushnotifications-8d42a",
   messagingSenderId: "677674369528",
   appId: "1:677674369528:web:1926ecb152fcb5a8e1160d",
 });
-
+onMessage(getMessaging(), (payload) => {
+  console.log("Message received. ", payload);
+  // ...
+});
 ReactDOM.render(
   <React.StrictMode>
     <App />
   </React.StrictMode>,
-  document.getElementById('root')
-)
+  document.getElementById("root")
+);
